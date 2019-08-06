ae82654401c5fc2aaafe6a24c38118763b1990c5
kevin <kithinjimkevin@gmail.com>
2019-08-06 14:10:09 +0300

    running ci ans

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='LibraryApp',
-    version='3.0.0',
+    version='3.0.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -30,6 +30,8 @@ setup(
         'Django>=1.11.22,<=2.2.2',
         'djangorestframework==3.9.4',
         'celery>=3.1.16'
+        'psycopg2>=2.8.3'
+        'psycopg2-binary>=2.8.3'
     ],
     scripts=[
         "bin/locallibrary_app"
